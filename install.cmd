1da67079529a629675c39c577e6ad339e565331f
Lei Duan <satifanie@gmail.com>
2012-08-23 21:54:00 +0800

    fix a bug by handwriting

--- install.cmd
+++ install.cmd
@@ -3,7 +3,7 @@
 
 @set BASE_DIR=%HOME%\.vim
 call git clone --recursive https://github.com/satifanie/ezVim.git %BASE_DIR%/ezVim
-call mkdir %BASE_DIR%\.vim\bundle
+call mkdir %BASE_DIR%\bundle
 
 call mklink %HOME%\.vimrc %BASE_DIR%\ezVim\_vimrc
 call mklink %HOME%\_vimrc %BASE_DIR%\ezVim\_vimrc
