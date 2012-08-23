f4e4e8c95809cd505530ca3881b15355733c1715
Lei Duan <satifanie@gmail.com>
2012-08-23 21:52:08 +0800

    add some files,and init my _vimrc

---
+++ install.cmd
@@ -0,0 +1,11 @@
+@if not exist "%HOME%" @set HOME=%HOMEDRIVE%%HOMEPATH%
+@if not exist "%HOME%" @set HOME=%USERPROFILE%
+
+@set BASE_DIR=%HOME%\.vim
+call git clone --recursive https://github.com/satifanie/ezVim.git %BASE_DIR%/ezVim
+call mkdir %BASE_DIR%\.vim\bundle
+
+call mklink %HOME%\.vimrc %BASE_DIR%\ezVim\_vimrc
+call mklink %HOME%\_vimrc %BASE_DIR%\ezVim\_vimrc
+
+call git clone http://github.com/gmarik/vundle.git %HOME%/.vim/bundle/vundle
